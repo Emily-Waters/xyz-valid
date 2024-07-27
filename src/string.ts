74797225262e19d8557599f73a25827e05d25a7c
Emily <emilyabigailwaters@gmail.com>
2024-07-26 17:13:00 -0700

    add min max for string

--- src/string.ts
+++ src/string.ts
@@ -1,3 +1,4 @@
+import XYZErrors from "./errors";
 import { XYZType } from "./type";
 
 function create() {
@@ -5,6 +6,25 @@ function create() {
     constructor() {
       super();
       this.primitive = "string";
+    }
+
+    min(n: number) {
+      this.checks.push((value) => {
+        if (value.length < n) {
+          this.errors.push(XYZErrors.invalidLength(value.length, n, "min"));
+        }
+      });
+
+      return this;
+    }
+    max(n: number) {
+      this.checks.push((value) => {
+        if (value.length > n) {
+          this.errors.push(XYZErrors.invalidLength(value.length, n, "max"));
+        }
+      });
+
+      return this;
     }
   }
 
