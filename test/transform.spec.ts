fdef61ac1eb00c2ec781db0289f145d19777251c
Emily <emilyabigailwaters@gmail.com>
2024-07-26 17:45:58 -0700

    add more tests

--- test/transform.spec.ts
+++ test/transform.spec.ts
@@ -10,4 +10,16 @@ describe("Transform", () => {
 
     expect(r).toBe(Number(s));
   });
+
+  it("should throw type error", () => {
+    const throwable = () => {
+      const s = 1;
+      const r = xyz
+        .string()
+        .transform((s) => Number(s))
+        .parse(s);
+    };
+
+    expect(throwable).toThrow(/Invalid Type:/);
+  });
 });
